13fa8d7d9846e9470508bd756fabe0eca3222b55
Nuria Padrosa Ortiz <npadrosa331@boscdelacoma.cat>
2021-12-17 16:33:00 +0100

    Unificar Pantalles

---
+++ WPFDesktop/AppDesktop/Admin.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppDesktop
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class Admin : Window
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPlayers_Click(object sender, RoutedEventArgs e)
+        {
+            Players objSecondWindow = new Players();
+            this.Visibility = Visibility.Hidden;
+            objSecondWindow.Show();
+        }
+
+        private void ButtonPokemons_Click(object sender, RoutedEventArgs e)
+        {
+            Pokemons objSecondWindow = new Pokemons();
+            this.Visibility = Visibility.Hidden;
+            objSecondWindow.Show();
+        }
+
+        private void ButtonCloseSession_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = System.Windows.MessageBox.Show("Vols tancar la sessió?", "Tancar sessió",  MessageBoxButton.YesNo);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    MainWindow objSecondWindow = new MainWindow();
+                    this.Visibility = Visibility.Hidden;
+                    objSecondWindow.Show();
+                    break;
+            }
+        }
+    }
+
+}
---
+++ WPFDesktop/AppDesktop/Pokemons.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+
+
+
+namespace AppDesktop
+{
+    /// <summary>
+    /// Lógica de interacción para Pokemons.xaml
+    /// </summary>
+    public partial class Pokemons : Window
+    {
+        public Pokemons()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPlayers_Click(object sender, RoutedEventArgs e)
+        {
+            Players objSecondWindow = new Players();
+            this.Visibility = Visibility.Hidden;
+            objSecondWindow.Show();
+        }
+
+        private void ButtonAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            Admin objSecondWindow = new Admin();
+            this.Visibility = Visibility.Hidden;
+            objSecondWindow.Show();
+        }
+
+        private void ButtonCloseSession_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = System.Windows.MessageBox.Show("Vols tancar la sessió?", "Tancar sessió", MessageBoxButton.YesNo);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    MainWindow objSecondWindow = new MainWindow();
+                    this.Visibility = Visibility.Hidden;
+                    objSecondWindow.Show();
+                    break;
+            }
+        }
+
+
+    }
+}
