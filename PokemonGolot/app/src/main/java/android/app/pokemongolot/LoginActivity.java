fb55a3be9b8736e726eaf349f2339f11c872e4e3
EloiLV <93603109+EloiLV@users.noreply.github.com>
2022-03-10 17:43:52 +0100

    Login Coded

--- PokemonGolot/app/src/main/java/android/app/pokemongolot/LoginActivity.java
+++ PokemonGolot/app/src/main/java/android/app/pokemongolot/LoginActivity.java
@@ -2,13 +2,33 @@ package android.app.pokemongolot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void logIn(View view) {
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.passwordUser);
+        //Intent intent = new Intent(this, MainActivity2.class);
+        if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+            //startActivity(intent);
+            Toast.makeText(getApplicationContext(), "Hola",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Dades incorrectes",Toast.LENGTH_SHORT).show();
+        }
     }
 }
