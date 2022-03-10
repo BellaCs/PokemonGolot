93f684949c38331ef72b5ce1b21e2dc708c36299
EloiLV <93603109+EloiLV@users.noreply.github.com>
2022-03-10 19:57:28 +0100

    Start Mongo API

---
+++ Server/ApiMongo/ApiMongo/Fight.cs
@@ -0,0 +1,15 @@
+namespace ApiMongo
+{
+    public class Fight
+    {
+        public DateTime Date { get; set; }
+
+        public string? player1 { get; set; }
+
+        public string? player2 { get; set; }
+
+        public string? pokemonPlayer1 { get; set; }
+
+        public string? pokemonPlayer2 { get; set; }
+    }
+}
