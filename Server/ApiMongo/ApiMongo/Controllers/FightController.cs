e0c8520e8c7e22cfa84e1755ad72ad760a460c80
EloiLV <93603109+EloiLV@users.noreply.github.com>
2022-03-11 17:49:42 +0100

    Added attacks

---
+++ Server/ApiMongo/ApiMongo/Controllers/FightController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiMongo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FightController : ControllerBase
+    {
+        private readonly ILogger<FightController> _logger;
+
+        public FightController(ILogger<FightController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<Fight> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Fight
+            {
+                Date = DateTime.Now.AddDays(index)
+            })
+            .ToArray();
+        }
+    }
+}
--- Server/ApiMongo/ApiMongo/Fight.cs
+++ Server/ApiMongo/ApiMongo/Fight.cs
@@ -11,5 +11,19 @@ namespace ApiMongo
         public string? pokemonPlayer1 { get; set; }
 
         public string? pokemonPlayer2 { get; set; }
+
+        public ICollection<Attacks> attack { get; set; }
+
+        public class Attacks
+        {
+            public string? att_name { get; set; }
+
+            public int att_dmg { get; set; }
+
+            public string? att_attacker { get; set; }
+
+            public string? att_defender { get; set; }
+
+        }
     }
 }
