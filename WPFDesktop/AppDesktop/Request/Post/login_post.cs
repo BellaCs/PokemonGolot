1b800dc8a1d02f4e4d61b0413b0740e6895a3d5f
Nuria Padrosa Ortiz <npadrosa331@boscdelacoma.cat>
2022-03-18 16:59:05 +0100

    Login

--- WPFDesktop/AppDesktop/Request/Post/login_post.cs
+++ WPFDesktop/AppDesktop/Request/Post/login_post.cs
@@ -3,24 +3,69 @@ using System.Collections.Generic;
 using System.Text;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Net;
+using System.Text.Json;
+using System.IO;
 
 namespace AppDesktop.Request.Post
 {
-    class login_post
+
+    
+
+    public class login_post
     {
-        //https://172.24.1.178:7292/Login/authenticate
+        // https://172.24.1.178:7292/Login/authenticate
 
         static readonly HttpClient client = new HttpClient();
 
-        public static async Task<string> getPokemons(string gen)
+
+        public class userLogin
+        {
+            public string user_name { get; set; }
+            public string password { get; set; }
+
+
+        }
+
+
+        public static string loginPost(string userNameLogin, string userPasswordLogin)
         {
             string sURL;
-            sURL = "https://172.24.1.178:7292/Login/authenticate";
-            var request = new HttpRequestMessage(HttpMethod.Get, sURL);
-            //request.Content ="test" ;
-
+            sURL = "https://172.24.1.178:7292/user/authenticate";
             try
             {
+                var request = WebRequest.Create(sURL);
+
+            request.Method = "POST";
+
+            // For serialize user data 
+            var userLogin = new userLogin
+            {
+                user_name = userNameLogin,
+                password = userPasswordLogin
+            };
+
+            var json = JsonSerializer.Serialize(userLogin);
+            byte[] byteArray = Encoding.UTF8.GetBytes(json);
+
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteArray.Length;
+
+            using var reqStream = request.GetRequestStream();
+            reqStream.Write(byteArray, 0, byteArray.Length);
+
+            using var response = request.GetResponse();
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+            using var respStream = response.GetResponseStream();
+
+            using var reader = new StreamReader(respStream);
+            string data = reader.ReadToEnd();
+            Console.WriteLine(data);
+
+            //record User(string Name, string Occupation);
+
+            
                 return "200";
             }
             catch(HttpRequestException e)
